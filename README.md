1edeb33b5adbd077fc6818001629fb2d77651497
Mikael Lönnroth <mikael@Mikaels-MacBook-Pro.local>
2019-12-31 18:45:10 +0200

    Added attitude corrections.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # TrueWind
 
-TrueWind is a small library for converting apparent wind to true wind based on speed and course over ground. 
+TrueWind is a small library for converting apparent wind to true wind. 
 
 ## Installation
 ```
@@ -26,6 +26,28 @@ console.log(
 ```
 
 **NOTE**: If true wind speed (tws) is 0, true wind angle (twd) will be ``undefined``.
+
+## Adjusting for pitch and roll
+
+If you include values for ``pitch`` and ``roll``, the function adjusts the return value for anemometer attitude. The math is grabbed from [this post](https://kingtidesailing.blogspot.com/2015/10/correcting-nmea-0183-wind-for-vessel.html).
+
+```
+const TrueWind = require("@gml/truewind");
+
+console.log(
+  TrueWind.getTrue({
+    aws: 10, // Apparent wind speed
+    awd: 45, // Apparent wind direction (0-359)
+    sog: 5.7, // Speed over ground
+    cog: 14, // Course over ground
+    pitch: 2, // Pitch in degrees 
+    roll: -15, // Roll in degrees
+  })
+);
+
+// Expected output
+// { tws: 5.97276396046677, twd: 76.12382083253168 }
+```
 
 ## Licensing
 
--- truewind.js
+++ truewind.js
@@ -1,40 +1,101 @@
+const r = Math.PI / 180.0;
+const d = 180.0 / Math.PI;
+
 class TrueWind {
-  /*  
-   * src: { sog, cog, aws, awd } 
-   * cog and awd in degrees (0-360)
+  /*
+   * s: {
+   *   sog // Speed over ground
+   *   cog // Course over ground
+   *   aws // Apparent wind speed
+   *   awd // Apparent wind direction (in degrees)
+   *   roll // Roll angle of sensor [optional]
+   *   pitch // Pitch angle of sensor [optional]
+   * }
    */
-  static getTrue(src) {
+  static getTrue(s) {
+    let src = this.getAttitudeCorrections(s);
+    //let src = Object.assign({}, s);
+    //console.log("TEMP:", JSON.stringify(src));
+
     if (!src.sog) {
       return {
         tws: src.aws,
-	twd: src.awd
-      }
+        twd: src.awd
+      };
     }
 
-    let cogR = src.cog * (Math.PI / 180);
-    let awdR = src.awd * (Math.PI / 180);
+    let cogR = src.cog * r;
+    let awdR = src.awd * r;
 
-    let u =
-      src.sog * Math.sin(cogR) -
-      src.aws * Math.sin(awdR);
+    let u = src.sog * Math.sin(cogR) - src.aws * Math.sin(awdR);
 
-    let v =
-      src.sog * Math.cos(cogR) -
-      src.aws * Math.cos(awdR);
+    let v = src.sog * Math.cos(cogR) - src.aws * Math.cos(awdR);
 
     let tws = Math.sqrt(u * u + v * v);
     let twd = undefined;
 
     if (tws > 0) {
-      twd =  v > 0
+      twd =
+        v > 0
           ? 180 + Math.atan(u / v) / (Math.PI / 180)
           : Math.atan(u / v) / (Math.PI / 180);
+    }
+
+    if (twd < 0) {
+      twd += 360;
     }
 
     return {
       tws: tws,
       twd: twd
     };
+  }
+
+
+  /*
+   * Correct for pitch and roll.
+   * This code is borrowed mostly from here:
+   * https://kingtidesailing.blogspot.com/2015/10/correcting-nmea-0183-wind-for-vessel.html
+   */
+  static getAttitudeCorrections(src) {
+    let roll = src.roll;
+    let pitch = src.pitch;
+
+    // Do nothing if we don't have roll and pitch.
+    if (roll === undefined || pitch === undefined) {
+      return src;
+    }
+
+    let awa = src.awd - src.cog;
+    if (awa < 0) {
+	awa += 360;
+    }
+ 
+    let rwa0 = awa;
+    let ws0 = src.aws;
+
+    let wx0 = ws0 * Math.sin(rwa0 * r);
+    let wy0 = ws0 * Math.cos(rwa0 * r);
+
+    // Skipping the rotational velocity adjustments for now
+    let wx1 = wx0;
+    let wy1 = wy0;
+
+    // Adjust for absolute roll and pitch
+    let wx2 = wx1 / Math.cos(roll * r);
+    let wy2 = wy1 / Math.cos(pitch * r);
+
+    let ws1 = Math.sqrt(Math.pow(wx2, 2) + Math.pow(wy2, 2));
+    let rwa1 = Math.atan2(wx2, wy2) * d;
+
+    if (rwa1 < 0) {
+      rwa1 += 360;
+    }
+
+    return Object.assign({}, src, {
+      awa: rwa1,
+      awd: (src.cog + rwa1) % 360
+    });
   }
 }
 
