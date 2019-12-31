82b5579a8a257a5745396263f60ca34191cd228e
Mikael Lönnroth <mikael@Mikaels-MacBook-Pro.local>
2019-12-31 13:47:33 +0200

    First draft.

---
+++ truewind.js
@@ -0,0 +1,37 @@
+class TrueWind {
+  static GetTrue(src) {
+    if (!src.sog) {
+      return {
+        tws: src.aws,
+	twd: src.awd
+      }
+    }
+
+    let cogR = src.cog * (Math.PI / 180);
+    let awdR = src.awd * (Math.PI / 180);
+
+    let u =
+      src.sog * Math.sin(cogR) -
+      src.aws * Math.sin(awdR);
+
+    let v =
+      src.sog * Math.cos(cogR) -
+      src.aws * Math.cos(awdR);
+
+    let tws = Math.sqrt(u * u + v * v);
+    let twd = undefined;
+
+    if (tws > 0) {
+      twd =  v > 0
+          ? 180 + Math.atan(u / v) / (Math.PI / 180)
+          : Math.atan(u / v) / (Math.PI / 180);
+    }
+
+    return {
+      tws: tws,
+      twd: twd
+    };
+  }
+}
+
+module.exports = TrueWind;
